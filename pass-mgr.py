0fb893bdf72b4d791b38b8be63d07e7d4f6f8937
Justin Ho <justinmh@hawaii.edu>
2016-07-19 23:22:35 -1000

    Got rid of commandline arguments
    added user options menu
    added function stubs for get_username() and get_identifier()
    implemented get_password() function

--- pass-mgr.py
+++ pass-mgr.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python
 
 import getopt
-from utils import *
+import utils
 
 
 def main(argv):
-    qmessage = 'Usage: pass-mgr -p <password to access the password manager>'
-    try:
-        opts, args = getopt.getopt(argv, "p:")
-    except getopt.GetoptError:
-        quitm(qmessage)
+    # Get the password from the user
+    passwd = utils.get_password()
 
-    if len(opts) < 1:
-        quitm(qmessage)
+    # print passwd
 
-    passwd = ''
+    # prompt the user
+    menu = '\nWhat would you like to do?\n' \
+           '1. Add Credentials\n' \
+           '2. Get Credentials\n' \
+           '3. Update Credentials\n' \
+           'Please enter the integer of your choice (1, 2, or 3): '
+    # user options
+    options = ['1', '2', '3']
 
-    #  get and set all the user defined options
-    for opt, arg in opts:
-        if opt == '-p':
-            passwd = arg
+    # Get the Users input option, input must be a valid integer 1, 2, or 3
+    user_option = raw_input(menu)
+    while user_option not in options:
+        print '[ERROR] Improper input, please enter a valid integer.'
+        user_option = raw_input(menu)
 
-    if not checkpasswd(passwd):
-        quitm(qmessage)
-
-    print passwd
+    if user_option == '1':
+        print 'You chose to Add Credentials!!'
+    elif user_option == '2':
+        print 'You chose to Get Credentials!!'
+    elif user_option == '3':
+        print 'You chose to Update Credentials!!'
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(utils.sys.argv[1:])
--- utils.py
+++ utils.py
@@ -1,11 +1,31 @@
 import sys
 
 
-def quitm(message):
+def quit_m(message):
     print message
     sys.exit(2)
 
 
-def checkpasswd(passwd):
-    # Empty method stub
+def check_passwd(passwd):
+    # Empty function stub
     return True
+
+
+def get_username():
+    # get the username from the user
+    return 'username'
+
+
+def get_password():
+    # authentication error message
+    auth_message = '[ERROR] Failed to authenticate. Incorrect password.'
+    # get the password from the user
+    password = raw_input('Please enter your password to access the password manager: ')
+    if not check_passwd(password):
+        quit_m(auth_message)
+    return password
+
+
+def get_identifier():
+    # get the identifier from the user
+    return 'identifier'
