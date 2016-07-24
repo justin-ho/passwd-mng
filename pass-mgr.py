fc1453f8a9429afa78dd61f11820c40dc6ca54b3
Brandon Arakaki <Brandon Arakaki>
2016-07-24 11:08:56 -1000

    Added code in add credentials, get credentials, and added an init

--- pass-mgr.py
+++ pass-mgr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import utils
+import UACC_Class
+import encryptedFileEditor
 import os
 
 
@@ -13,7 +15,8 @@ def main():
         utils.authenticate()
     else:
         utils.new_passwd(utils.get_passwd())
-
+#add init here
+    encryptedFileEditor.init()
     # Menu that the user will see
     menu = '\nWhat would you like to do?\n\n' \
            '1. Add Credentials\n' \
@@ -38,13 +41,27 @@ def main():
         # Perform the action based on the users choice
         if user_option == '1':
             print '********Add Credentials********'
-            utils.get_identifier()
-            utils.get_username()
-            utils.get_passwd()
+            #add uacc object and pass to elliot
+            identifiercheck = utils.get_identifier()
+            usernamecheck = utils.get_username()
+            passwordcheck = utils.get_passwd()
+            userinformation = UACC_Class.UACC(identifiercheck, usernamecheck, passwordcheck)
+            if(UACC_Class.UACC.identifier_is_valid(identifiercheck) == True):
+                if(UACC_Class.Uacc.username_is_valid(usernamecheck)):
+                    if(UACC_Class.UACC.password_is_valid(passwordcheck)):
+                        encryptedFileEditor.add_user(identifiercheck, UACC_Class.Uacc.tostring(userinformation))
+                    else:
+                        print "Does not meet requirements. Make sure it is 8-16 characters long, and no whitespace"
+                else:
+                    print "Does not meet requirements. Make sure it has less than 16 characters, no whitespace, and is not blank"
+            else:
+                "Does not meet requirements. Make sure it has no digits, white space, and is not blank. "
         elif user_option == '2':
             print '********Get Credentials********'
             # Use the identifier to get the username and password for the authenticated user
-            utils.get_identifier()
+            getidentifier = utils.get_identifier()
+            encryptedFileEditor.get_user_info(getidentifier)
+
         elif user_option == '3':
             print '********Update Credentials********'
             # Use the identifier and update the username and password for that identifier
