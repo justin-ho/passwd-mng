88586ce5b48a316377489f9302bbcc1bf57313af
Brandon Arakaki <Brandon Arakaki>
2016-07-23 14:29:32 -1000

    Changed class functions to return true or false based on input

--- UACC_Class.py
+++ UACC_Class.py
@@ -16,23 +16,26 @@ class UACC:
         #not identifier checks to see if the user inputs something
         #re.search(r'[\s]', identifier)checks if there is any whitespace in the user's input
     def identifier_is_valid(self):
-        while(any(str.isdigit(c) for c in self.identifier) == True or not self.identifier or re.search(r'[\s]', self.identifier)):
-            self.identifier = raw_input("Please insert an identifier that contains no numbers, contains atleast one character, and no white space. ")
-        return True
+        if(any(str.isdigit(c) for c in self.identifier) == True or not self.identifier or re.search(r'[\s]', self.identifier)):
+            return False
+        else:
+            return True
 
 #This function checks to see if the username meets our requirements. Username must not be: blank and no longer than 16 characters long
         #len(username) checks the length of the input
     def username_is_valid(self):
-        while(not self.username or len(self.username) > 16 or re.search(r'[\s]', self.username)):
-            self.username = raw_input("Please insert a username that contains atleast one character, less than 16 characters long, and no white space. ")
-        return True
+        if(not self.username or len(self.username) > 16 or re.search(r'[\s]', self.username)):
+            return False
+        else:
+            return True
 
 #This function checks to see if the password meets our requirements.
 # Password must not be: blank and no longer than 16 characters long and not shorter than 8 characters long
     def password_is_valid(self):
-       while(len(self.password) > 16 or len(self.password)< 8 or re.search(r'[\s]', self.password)):
-           self.password = raw_input("Please insert a password that contains 8-16 characters and uses no white space.")
-       return True
+       if(len(self.password) > 16 or len(self.password)< 8 or re.search(r'[\s]', self.password)):
+            return False
+       else:
+            return True
 
 #This tostring prints out the information that it has.
     def tostring(self):
@@ -40,10 +43,11 @@ class UACC:
 
 
 #Below is test code to test if class is working
-#def main():
-#    user1 = UACC("", "", "")
-#    UACC.identifier_is_valid(user1)
-#    UACC.username_is_valid(user1)
-#    UACC.password_is_valid(user1)
-#    UACC.tostring(user1)
-#main()
+def main():
+    user1 = UACC("", "", "")
+    UACC.username_is_valid(user1)
+    UACC.tostring(user1)
+
+
+if __name__ == "__main__":
+    main()
