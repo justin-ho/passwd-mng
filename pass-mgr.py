ec86541eb770d2ee33283ad248bdcee63fc13315
Brandon Arakaki <Brandon Arakaki>
2016-08-03 21:55:08 -1000

    Added wait to other options

--- pass-mgr.py
+++ pass-mgr.py
@@ -100,7 +100,7 @@ def main():
                     user_array[2] = ""
                 else:
                     print "Identifier not found."
-                print "\n Press any key to continue..."
+                print "\nPress any key to continue..."
                 wait()
 
             elif user_option == '3':
@@ -110,10 +110,14 @@ def main():
                 # Get the new username and password to update
                 utils.get_username()
                 utils.get_passwd()
+                print "\nPress any key to continue..."
+                wait()
             elif user_option == '4':
                 print '********Remove Credentials********'
                 # Use the identifier and remove the credentials from the datastore
                 utils.get_identifier()
+                print "\nPress any key to continue..."
+                wait()
     except authenticationError:
         print '[ERROR] Failed to authenticate. Max amount of tries reached.'
     except KeyboardInterrupt:
